60bcea7aae435178cc893195c0b8afd43d8aeb1b
Harry Prior <harryprior@hotmail.co.uk>
2020-11-03 19:09:57 +0000

    Setup inital interface and class for recursive html scrape

---
+++ Scraper/RecursiveWikiScraper/GetRecusiveWikiHtml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scraper.RecursiveWikiScraper
+{
+    class GetRecusiveWikiHtml : IGetRecursiveHtml
+    {
+        public IList<string> GetHtmlStrings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Scraper/RecursiveWikiScraper/IGetRecursiveHtml.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scraper.RecursiveWikiScraper
+{
+    interface IGetRecursiveHtml
+    {
+        IList<string> GetHtmlStrings();
+    }
+}
