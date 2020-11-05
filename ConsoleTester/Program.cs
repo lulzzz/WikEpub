061ae44d5515ecc69c64218715e60a50dfd1374d
Harry Prior <harryprior@hotmail.co.uk>
2020-11-05 11:14:04 +0000

    start removing f# to restart

--- ConsoleTester/Program.cs
+++ ConsoleTester/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using FSharp.Data;
+using Microsoft.FSharp.Core;
+using Microsoft.VisualBasic;
+using System;
+using WebScraper;
 
 namespace ConsoleTester
 {
@@ -6,7 +10,12 @@ namespace ConsoleTester
     {
         static void Main(string[] args)
         {
+            HtmlDocument doc = HtmlDocument.Load("https://www.google.com");
+            Console.WriteLine(doc.Body());
+            Say.hello("Harry");
             Console.WriteLine("Hello World!");
         }
     }
 }
+
+
