967c82ef521949b3366bcdd490d061df75417c92
Harry Prior <harryprior@hotmail.co.uk>
2020-11-06 10:13:40 +0000

    Add FsharpWikiLibrary

--- WikEpubLibrary/Library.fs
+++ WikEpubLibrary/Library.fs
@@ -1,5 +1,12 @@
 ﻿namespace WikEpubLibrary
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open FSharp.Data
+
+module ScrapeWiki =
+    let html = HtmlDocument.Load("https://en.wikipedia.org/wiki/Donald_Trump")
+
+    let getTagAttributes tag attribute (doc:HtmlDocument) = 
+        doc.Descendants [tag]
+        |> Seq.choose (fun x -> 
+            x.TryGetAttribute(attribute)) |> Seq.map(fun x -> x.Value())
+   
