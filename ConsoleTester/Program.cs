b325564c4762c5a24c918a4840625ef786c1046c
Harry Prior <harryprior@hotmail.co.uk>
2020-11-06 10:16:20 +0000

    Remove debug code and reference to old library name

--- ConsoleTester/Program.cs
+++ ConsoleTester/Program.cs
@@ -1,7 +1,6 @@
 ﻿using FSharp.Data;
 using System;
 using System.Linq;
-using WikEpubLibrary;
 namespace ConsoleTester
 {
     class Program
@@ -9,10 +8,6 @@ namespace ConsoleTester
         static void Main(string[] args)
         {
 
-            HtmlDocument htmlDocument = HtmlDocument.Load("https://en.wikipedia.org/wiki/Example_(musician)");
-            //     Console.WriteLine(htmlDocument);
-            var newDoc = ScrapeWiki.getNewHtml(htmlDocument);
-            Console.Write(newDoc.ToString());
         }
     }
 }
