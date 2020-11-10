b6c68b002b6534a6c992b9cce411f1061fd9c4fe
Harry Prior <harryprior@hotmail.co.uk>
2020-11-10 20:47:47 +0000

    remove unused f# libary module

--- WikEpubLibrary/Library.fs
+++ WikEpubLibrary/Library.fs
@@ -2,11 +2,5 @@
 
 open FSharp.Data
 
-module ScrapeWiki =
-    let html = HtmlDocument.Load("https://en.wikipedia.org/wiki/Donald_Trump")
-
-    let getTagAttributes tag attribute (doc:HtmlDocument) = 
-        doc.Descendants [tag]
-        |> Seq.choose (fun x -> 
-            x.TryGetAttribute(attribute)) |> Seq.map(fun x -> x.Value())
-   
+    
+       
