3c76fdb970d85ee5926d6cbb1669279e155eb73a
Harry Prior <harryprior@hotmail.co.uk>
2020-11-10 23:32:46 +0000

    Add interface for downloading files

---
+++ CSharpWikEpubLibrary/ProcessHtml/IDownloadFiles.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace CSharpWikEpubLibrary.ScrapeWiki
+{
+    public interface IDownloadFiles
+    {
+        public void DownloadFiles(IEnumerable<string> fromUrl, string toDirectory);
+    }
+}
