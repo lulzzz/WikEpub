d7700f11678866f96be59c4fb96ed8faaf004f32
Harry Prior <harryprior@hotmail.co.uk>
2020-11-10 20:46:26 +0000

    Add classes to get HTML and return epub version

---
+++ CSharpWikEpubLibrary/ProcessHtml/IGetHtmlDoc.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace CSharpWikEpubLibrary.ScrapeWiki
+{
+    public interface IGetHtmlDoc
+    {
+        HtmlDocument GetHtmlDocument(HtmlDocument inputDocument);
+    }
+}
