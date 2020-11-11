88293cb4296cc455278ccf02cb1b48d23339992e
Harry Prior <harryprior@hotmail.co.uk>
2020-11-11 16:48:04 +0000

    Make TransformHtml -> Epub method more general purpose. Rename to GetEpub

---
+++ CSharpWikEpubLibrary/ProcessHtml/GetEpub.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSharpWikEpubLibrary.ScrapeWiki;
+using HtmlAgilityPack;
+
+namespace CSharpWikEpubLibrary.ProcessHtml
+{
+    public class GetEpub : ITransformHtmlDoc
+    {
+        public HtmlDocument Transform(HtmlDocument inputDocument)
+        {
+            bool HeadPredicate(HtmlNode node) => (node.Name == "meta" & node.Attributes.Any(attribute => attribute.Name == "charset")) 
+                                                 | node.Name == "title";
+
+            var bodyString = GetHtmlString(inputDocument, "//*[@id='mw-content-text']/div[1]", node => true, "body");
+            var headString = GetHtmlString(inputDocument, "//html/head", HeadPredicate, "head");
+            var htmlString = 
+                string.Join(
+                    "", 
+                    new List<string>{headString, bodyString}
+                        .Prepend("<!DOCTYPE html><html>")
+                        .Append("</html>")
+                    );
+            HtmlDocument newDoc = new HtmlDocument();
+            newDoc.LoadHtml(htmlString);
+            return newDoc;
+        }
+       
+
+        private string GetHtmlString(HtmlDocument inputDocument, string xPathToParent, Predicate<HtmlNode> nodePredicate,
+            string encapsulateWithNode)
+        {
+            var nodeStrings = new List<string>();
+            var currentNode = inputDocument.DocumentNode.SelectSingleNode(xPathToParent).FirstChild;
+            while (currentNode != null)
+            {
+                if (nodePredicate(currentNode))
+                {
+                    nodeStrings.Add(currentNode.OuterHtml);
+                }
+                currentNode = currentNode.NextSibling;
+            }
+
+            return string.Join("", nodeStrings.Prepend($"<{encapsulateWithNode}>").Append($"</{encapsulateWithNode}"));
+        }
+
+
+       
+    }
+}
