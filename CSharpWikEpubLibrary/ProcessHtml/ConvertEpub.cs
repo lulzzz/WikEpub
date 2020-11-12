c30084609ea0ec0e2f54b9b7be7648d545cafcfb
Harry Prior <harryprior@hotmail.co.uk>
2020-11-12 16:30:06 +0000

    Add main access point to library - 'GetEpub'

---
+++ CSharpWikEpubLibrary/ProcessHtml/ConvertEpub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace CSharpWikEpubLibrary.ProcessHtml
+{
+    class ConvertEpub : IConvertEpub
+    {
+        private IParseHtmlDoc _getInitHtml;
+        private IProcessImages _processImages;
+        public ConvertEpub(IParseHtmlDoc getInitHtml, IProcessImages processImages)
+        {
+            _getInitHtml = getInitHtml;
+            _processImages = processImages;
+        }
+        public void Convert(IEnumerable<string> urls)
+        {
+            throw new NotImplementedException();
+
+        }
+        
+        private int _htmlId;
+        private string HtmlId => $"doc_{_htmlId++}";
+        private Dictionary<HtmlDocument,string> GetHtmlId(IEnumerable<HtmlDocument> htmlDocuments) =>
+            htmlDocuments.ToDictionary( keySelector: document =>  document, document => HtmlId );
+        
+
+    }
+
+}
---
+++ CSharpWikEpubLibrary/ProcessHtml/IConvertEpub.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+namespace CSharpWikEpubLibrary.ProcessHtml
+{
+    public interface IConvertEpub
+    {
+        void Convert(IEnumerable<string> urls);
+    }
+}
