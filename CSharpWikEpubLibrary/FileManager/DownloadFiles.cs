46235ae026fbac80892680b0a7e49070216bcb8d
Harry Prior <harryprior@hotmail.co.uk>
2020-11-12 11:26:38 +0000

    Move Download files to FileManager namespace

--- CSharpWikEpubLibrary/FileManager/DownloadFiles.cs
+++ CSharpWikEpubLibrary/FileManager/DownloadFiles.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.IO.Enumeration;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
-using CSharpWikEpubLibrary.ScrapeWiki;
 
-namespace CSharpWikEpubLibrary.ProcessHtml
+namespace CSharpWikEpubLibrary.FileManager
 {
     public class DownloadFiles : IDownloadFiles
     {
---
+++ CSharpWikEpubLibrary/FileManager/IDownloadFiles.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpWikEpubLibrary.FileManager
+{
+    public interface IDownloadFiles
+    {
+        public void  DownloadAsync(IEnumerable<string> fromUrls, string toDirectory);
+    }
+}
