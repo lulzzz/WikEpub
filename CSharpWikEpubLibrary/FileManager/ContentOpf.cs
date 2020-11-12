d2f3844a91509f1be3842e5d189ea97530df90b6
Harry Prior <harryprior@hotmail.co.uk>
2020-11-12 16:28:39 +0000

    Add class to create Opf doc

---
+++ CSharpWikEpubLibrary/FileManager/ContentOpf.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Xml;
+
+namespace CSharpWikEpubLibrary.FileManager
+{
+    class ContentOpf : IContentOpf
+    {
+        public void Create(Dictionary<HtmlDocument, string> htmlInfo, string inDirectory, string bookTitle)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        IEnumerable<HtmlNode> DocumentNodes(HtmlDocument document) => document.DocumentNode.Descendants();
+
+
+        private List<string> GetImagePaths(IEnumerable<HtmlNode> nodes) =>
+            nodes
+                .Where(node => node.Name == "img")
+                .Select(node => node.GetAttributeValue("src", "no_value")).Distinct()
+                .Select(path => string.Join(@"\", path.Split(@"\")[^2..]))
+                .ToList();
+    }
+}
---
+++ CSharpWikEpubLibrary/FileManager/IContentOpf.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace CSharpWikEpubLibrary.FileManager
+{
+    public interface IContentOpf
+    {
+        public void Create(Dictionary<HtmlDocument, string> htmlInfo, string directory, string bookTitle);
+
+    }
+}
