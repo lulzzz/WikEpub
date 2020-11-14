45ea2c5e50b8c5dcbf560f68d968a3776ba4194b
Harry Prior <harryprior@hotmail.co.uk>
2020-11-14 13:07:57 +0000

    Add class to create Toc file

---
+++ CSharpWikEpubLibrary/FileManager/IToc.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace CSharpWikEpubLibrary.FileManager
+{
+    public interface IToc
+    {
+        void Create(Dictionary<HtmlDocument, string> htmlIds, string toDirectory);
+    }
+}
---
+++ CSharpWikEpubLibrary/FileManager/Toc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using HtmlAgilityPack;
+
+namespace CSharpWikEpubLibrary.FileManager
+{
+    public class Toc: IToc
+    {
+        public void Create(Dictionary<HtmlDocument, string> htmlIds, string toDirectory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public XDocument GetTocDocument(Dictionary<HtmlDocument, string> htmlIds)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
