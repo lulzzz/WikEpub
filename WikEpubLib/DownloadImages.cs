a33ec4c73a9723a040417d942a80c624335d5031
Harry Prior <harryprior@hotmail.co.uk>
2020-11-16 22:33:07 +0000

    Add class for downloading images to

---
+++ WikEpubLib/DownloadImages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikEpubLib
+{
+    public class DownloadImages : IDownloadImages
+    {
+        public async Task From(WikiPageRecord pageRecord, string oepbsDirectory, HttpClient httpClient)=>
+            await pageRecord.SrcMap.ToList().ForEachAsync(async src =>
+            {
+                string htmlImageRepository = src.Value.Split('\\')[0];
+                string filePath = @$"{oepbsDirectory}\{src.Value}";
+
+                HttpResponseMessage responseResult = await httpClient.GetAsync(@$"https://{src.Key}");
+                using var memoryStream = await responseResult.Content.ReadAsStreamAsync();
+                Directory.CreateDirectory(@$"{oepbsDirectory}\{htmlImageRepository}\");
+                await using var fileStream = File.Create(filePath);
+                await memoryStream.CopyToAsync(fileStream);
+            });
+         
+    }
+}
