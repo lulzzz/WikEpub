aefa6fee8107996c116681b1fd996d98a6bd4eb1
Harry Prior <harryprior@hotmail.co.uk>
2020-11-16 11:52:10 +0000

    Add record type for wikipage

---
+++ WikEpubLib/WikiPageRecord.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WikEpubLib
+{
+    public record WikiPageRecord
+    {
+        string Id { get; init; }
+        Dictionary<string, string> SrcMap { get; init; }
+        IEnumerable<string> SectionHeadings { get; init; }
+    }
+}
