6a00658fd82dc1a4a1e3318d6df2e0f4946bef8c
Harry Prior <harryprior@hotmail.co.uk>
2020-11-16 22:42:25 +0000

    Add interface for HtmlToEpub

---
+++ WikEpubLib/Interfaces/IHtmlsToEpub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikEpubLib.Interfaces
+{
+    public interface IHtmlsToEpub
+    {
+        public Task Transform(IEnumerable<string> withUrls, string toRootDirectory, string asBookTitle);
+    }
+}
