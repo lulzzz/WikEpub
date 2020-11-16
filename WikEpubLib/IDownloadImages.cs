afda0c54bd08df1bd155ce5e65b4f9f106d630a3
Harry Prior <harryprior@hotmail.co.uk>
2020-11-16 22:33:18 +0000

    Add interface

---
+++ WikEpubLib/IDownloadImages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikEpubLib
+{
+    public interface IDownloadImages
+    {
+        public Task From(WikiPageRecord pageRecord, string oepbsDirectory, HttpClient httpClient);
+    }
+}
