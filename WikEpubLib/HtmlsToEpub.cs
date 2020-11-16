4246c899e2ff90adcbce8ff4e298b19833b5c4fb
Harry Prior <harryprior@hotmail.co.uk>
2020-11-16 21:36:56 +0000

    Add preliminary main setup

--- WikEpubLib/HtmlsToEpub.cs
+++ WikEpubLib/HtmlsToEpub.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace WikEpubLib
 {
     class HtmlsToEpub
     {
-        public void Transform(IEnumerable<string> urls, string toRootDirectory)
+        IParseHtml _parseHtml;
+        IGetWikiPageRecords _getRecords;
+
+        public HtmlsToEpub(IParseHtml parseHtml, IGetWikiPageRecords getRecords)
         {
+            _parseHtml = parseHtml;
+            _getRecords = getRecords;
+        }
+
+        public async Task Transform(IEnumerable<string> withUrls, string toRootDirectory, string asBookTitle)
+        {
+            HtmlDocument[] initialDocs = await GetHtmlDocuments(withUrls);
+
+            int imgDirNum = 1;
+            string GetImageDir() => $"image_directory_{imgDirNum++}";
+            IEnumerable<(HtmlDocument html, WikiPageRecord pageRecord)> docRecordDict = 
+                initialDocs.AsParallel().Select(doc => (doc, _getRecords.From(doc, GetImageDir())));
+
+            // do IO bound work here
+            IEnumerable<HtmlDocument> parsedDocuments = docRecordDict.AsParallel().Select(t => _parseHtml.Parse(t.html, t.pageRecord));
+                
             throw new NotImplementedException();
         }
+
+        private async Task<HtmlDocument[]> GetHtmlDocuments(IEnumerable<string> urls)
+        {
+            HtmlWeb htmlWeb = new HtmlWeb();
+            return await Task.WhenAll(urls.Select(url => htmlWeb.LoadFromWebAsync(url)));
+        }
+
+        
+        
     }
 }
