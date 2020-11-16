a652390417be0a91061cdcf70131d43e6a40d8c7
Harry Prior <harryprior@hotmail.co.uk>
2020-11-16 13:23:28 +0000

    Add test for record id

--- WikEpubLibTests/GetWikiPageRecordsTests.cs
+++ WikEpubLibTests/GetWikiPageRecordsTests.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WikEpubLib;
+using HtmlAgilityPack;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WikEpubLibTests
 {
-    class GetWikiPageRecordsTests
+    [TestClass]
+    public class GetWikiPageRecordsTests
     {
-        [TestClass]
-        public class UnitTest1
+        HtmlWeb webGet = new HtmlWeb();
+        GetWikiPageRecords GetWikiPageRecords = new GetWikiPageRecords();
+        string seanConnWiki = "https://en.wikipedia.org/wiki/Sean_Connery";
+        string seanImageDir = "image_dir1";
+        WikiPageRecord seanRecord;
+        
+        [TestInitialize]
+        public void InitialiseTest()
         {
-            [TestMethod]
-            public void TestMethod1()
-            {
-            }
+            var seanWikiDoc = webGet.Load(seanConnWiki);
+            seanRecord = GetWikiPageRecords.GetRecordsFrom(seanWikiDoc, seanImageDir);
         }
 
+        [TestMethod]
+        public void Corrent_ID_Sean_Connery()
+        {
+            Assert.AreEqual("Sean_Connery", seanRecord.Id);
+        }
+
+
+
+        
     }
+
 }
