2e55722651fda1b6d896bd5ddeda8b258fbc2878
Harry Prior <harryprior@hotmail.co.uk>
2020-11-16 13:36:40 +0000

    Indented method chaining for readability

--- WikEpubLib/GetWikiPageRecords.cs
+++ WikEpubLib/GetWikiPageRecords.cs
@@ -20,14 +20,19 @@ namespace WikEpubLib
         }
 
         private string GetIdFrom(IEnumerable<HtmlNode> nodes) =>
-            nodes.First(n => n.Name == "title").InnerHtml.Split('-').First().Trim().Replace(' ', '_').Replace(")", "").Replace("(", "");
+            nodes
+            .First(n => n.Name == "title")
+            .InnerHtml.Split('-').First()
+            .Trim().Replace(' ', '_').Replace(")", "").Replace("(", "");
 
         private IEnumerable<HtmlNode> GetImageNodesFrom(IEnumerable<HtmlNode> nodes) => nodes.Where(n => n.Name == "img");
 
         private int _imageId = 1;
         private string GetImageId(string originalSrc) => $"image_{_imageId++}.{originalSrc.Split('.')[^1]}";
         private Dictionary<string, string> GetSrcMapFrom(IEnumerable<HtmlNode> imageNodes, string imageDirectory) =>
-            imageNodes.Select(n => n.GetAttributeValue("src", "null")).Distinct().ToDictionary(s => s, s => @$"{imageDirectory}\{GetImageId(s)}");
+            imageNodes
+            .Select(n => n.GetAttributeValue("src", "null"))
+            .Distinct().ToDictionary(s => s, s => @$"{imageDirectory}\{GetImageId(s)}");
 
         private IEnumerable<(string id, string sectionName)> GetSectionHeadingsFrom(IEnumerable<HtmlNode> nodes) =>
             nodes
