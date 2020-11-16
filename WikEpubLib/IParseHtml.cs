d709bdef5cd7a6f75cbbfc63aa4347362d17614e
Harry Prior <harryprior@hotmail.co.uk>
2020-11-16 21:38:47 +0000

    Add interface for parsing html

---
+++ WikEpubLib/IParseHtml.cs
@@ -0,0 +1,15 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikEpubLib
+{
+    interface IParseHtml
+    {
+        public HtmlDocument Parse(HtmlDocument htmlDocument, WikiPageRecord wikiPageRecord);
+
+    }
+}
