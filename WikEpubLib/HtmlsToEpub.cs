39450ec5ab8835372504b2743c8b0c570c30f855
Harry Prior <harryprior@hotmail.co.uk>
2020-11-16 19:36:06 +0000

    Add 'main' class for converting html to epub

---
+++ WikEpubLib/HtmlsToEpub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikEpubLib
+{
+    class HtmlsToEpub
+    {
+        public void Transform(IEnumerable<string> urls, string toRootDirectory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
