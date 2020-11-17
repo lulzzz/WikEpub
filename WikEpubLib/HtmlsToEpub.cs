fabcc4cb242048066a8e10a95c634ce5194fa95e
Harry Prior <harryprior@hotmail.co.uk>
2020-11-17 17:09:03 +0000

    Add methods for converting html to epub

--- WikEpubLib/HtmlsToEpub.cs
+++ WikEpubLib/HtmlsToEpub.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using WikEpubLib.Interfaces;
+using System.IO;
 
 namespace WikEpubLib
 {
@@ -21,26 +22,54 @@ namespace WikEpubLib
 
         public async Task Transform(IEnumerable<string> withUrls, string toRootDirectory, string asBookTitle)
         {
-            HtmlDocument[] initialDocs = await GetHtmlDocuments(withUrls);
+            HtmlDocument[] initialDocs = await GetHtmlDocuments(withUrls, new HtmlWeb());
 
-            int imgDirNum = 1;
-            string GetImageDir() => $"image_directory_{imgDirNum++}";
-            IEnumerable<(HtmlDocument html, WikiPageRecord pageRecord)> docRecordDict = 
-                initialDocs.AsParallel().Select(doc => (doc, _getRecords.From(doc, GetImageDir())));
+
+            // create directorys async
+            Guid guid = Guid.NewGuid();
+            Task createDirectories = CreateDirectories(toRootDirectory, guid);
+            var directories = GetDirectoryDict(toRootDirectory, guid);
+
+            IEnumerable<(HtmlDocument html, WikiPageRecord pageRecord)> htmlRecordTuple = 
+                initialDocs.AsParallel().Select(doc => (doc, _getRecords.From(doc, "image_directory")));
+
+
+            // wait for directories
+            
+            // create xml files
 
             // do IO bound work here
-            IEnumerable<HtmlDocument> parsedDocuments = docRecordDict.AsParallel().Select(t => _parseHtml.Parse(t.html, t.pageRecord));
-                
+            IEnumerable<HtmlDocument> parsedDocuments = 
+                htmlRecordTuple.AsParallel().Select(t => _parseHtml.Parse(t.html, t.pageRecord));
+            
+            // save files here
+            
             throw new NotImplementedException();
         }
 
-        private async Task<HtmlDocument[]> GetHtmlDocuments(IEnumerable<string> urls)
-        {
-            HtmlWeb htmlWeb = new HtmlWeb();
-            return await Task.WhenAll(urls.Select(url => htmlWeb.LoadFromWebAsync(url)));
-        }
+        private async Task<HtmlDocument[]> GetHtmlDocuments(IEnumerable<string> urls, HtmlWeb htmlWeb) =>
+            await Task.WhenAll(urls.Select(url => htmlWeb.LoadFromWebAsync(url)));
 
-        
-        
+        private async Task CreateDirectories(string rootDirectory, Guid id) =>
+            await Task.Run(() =>
+            {
+                Directory.CreateDirectory(@$"{rootDirectory}\{id}\OEBPS");
+                Directory.CreateDirectory(@$"{rootDirectory}\{id}\META-INF");
+            });
+
+        private Dictionary<Directories, string> GetDirectoryDict(string rootDir, Guid id) => new Dictionary<Directories, string> {
+            {Directories.ROOT, rootDir},
+            {Directories.OEBPS, @$"{rootDir}\{id}\OEBPS" },
+            {Directories.METAINF, @$"{rootDir}\{id}\META-INF" },
+            {Directories.BOOKDIR,  @$"{rootDir}\{id}" }
+        };
+    }
+
+    internal enum Directories
+    {
+        ROOT,
+        OEBPS,
+        METAINF,
+        BOOKDIR
     }
 }
