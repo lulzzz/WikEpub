43bf473e0e154198e5c891e2733e7c25a233fe18
Harry Prior <harryprior@hotmail.co.uk>
2020-11-17 14:09:43 +0000

    Extract record looping logic into method

--- WikEpubLib/GetTocXml.cs
+++ WikEpubLib/GetTocXml.cs
@@ -1,57 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Xml.Linq;
 using WikEpubLib.Interfaces;
+
 namespace WikEpubLib
 {
     public class GetTocXml : IGetTocXml
     {
         public XDocument From(IEnumerable<WikiPageRecord> pageRecords, string bookTitle)
         {
-            int playOrder = 1;
-            string GetPlayOrder() => (playOrder++).ToString();
             XNamespace defaultNs = "http://www.daisy.org/z3986/2005/ncx/";
 
             XElement ncx = new XElement(
                 defaultNs + "ncx",
-                new XAttribute("version","2005-1"),
+                new XAttribute("version", "2005-1"),
                 new XElement(
                         defaultNs + "head",
                         new XElement(
                             defaultNs + "meta",
                             new XAttribute("name", "cover"),
                             new XAttribute("content", "cover")
-                            )
-                        )
-                );
+                            )));
+
             XElement docTitle = new XElement(
                 defaultNs + "docTitle",
                 new XElement(defaultNs + "text", bookTitle)
                 );
-            
+
+            ncx.Add(docTitle);
+            ncx.Add(GetNavMap(pageRecords, defaultNs));
+            return new XDocument(ncx);
+        }
+
+        private XElement GetNavMap(IEnumerable<WikiPageRecord> pageRecords, XNamespace defaultNs)
+        {
+            int playOrder = 1;
+            string GetPlayOrder() => (playOrder++).ToString();
             XElement navMap = new XElement(defaultNs + "navMap");
 
-            foreach(var record in pageRecords)
+            foreach (var record in pageRecords)
             {
                 XElement docNavPoint = GetNavPoint(defaultNs, record.Id, GetPlayOrder());
-                foreach(var recordSection in record.SectionHeadings)
+                foreach (var recordSection in record.SectionHeadings)
                 {
                     XElement section = GetNavPoint(defaultNs, record.Id, GetPlayOrder(), recordSection.sectionName, recordSection.id);
                     docNavPoint.Add(section);
                 }
                 navMap.Add(docNavPoint);
             }
-            ncx.Add(docTitle);
-            ncx.Add(navMap);
-            return new XDocument(ncx);
-
+            return navMap;
         }
 
-
-        XElement GetNavPoint(XNamespace defaultNs, string id, string playOrder) =>
+        private XElement GetNavPoint(XNamespace defaultNs, string id, string playOrder) =>
             new XElement(
                 defaultNs + "navPoint",
                 new XAttribute("id", id),
@@ -60,7 +59,7 @@ namespace WikEpubLib
                 new XElement(defaultNs + "content", new XAttribute("src", $"{id}.html"))
                 );
 
-        XElement GetNavPoint(XNamespace defaultNs, string id, string playOrder, string sectionName, string hashId) =>
+        private XElement GetNavPoint(XNamespace defaultNs, string id, string playOrder, string sectionName, string hashId) =>
             new XElement(
                 defaultNs + "navPoint",
                 new XAttribute("id", id),
