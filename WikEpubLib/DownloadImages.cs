903e8cb044c0763f144ca2ae25be94720112f6e2
Harry Prior <harryprior@hotmail.co.uk>
2020-11-17 10:39:37 +0000

    Remove httpClient from method signature

--- WikEpubLib/DownloadImages.cs
+++ WikEpubLib/DownloadImages.cs
@@ -17,13 +17,13 @@ namespace WikEpubLib
         {
             _httpClient = httpClient;
         }
-        public async Task From(WikiPageRecord pageRecord, string oepbsDirectory, HttpClient httpClient)=>
+        public async Task From(WikiPageRecord pageRecord, string oepbsDirectory) =>
             await pageRecord.SrcMap.ToList().ForEachAsync(async src =>
             {
                 string htmlImageRepository = src.Value.Split('\\')[0];
                 string filePath = @$"{oepbsDirectory}\{src.Value}";
 
-                HttpResponseMessage responseResult = await httpClient.GetAsync(@$"https://{src.Key}");
+                HttpResponseMessage responseResult = await _httpClient.GetAsync(@$"https://{src.Key}");
                 using var memoryStream = await responseResult.Content.ReadAsStreamAsync();
                 Directory.CreateDirectory(@$"{oepbsDirectory}\{htmlImageRepository}\");
                 await using var fileStream = File.Create(filePath);
