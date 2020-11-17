c6b8f80e104dcc62104f75f3ddab1c52ab5a903d
Harry Prior <harryprior@hotmail.co.uk>
2020-11-17 13:13:16 +0000

    Change interface names

---
+++ WikEpubLib/Interfaces/IGetTocXml.cs
@@ -0,0 +1,15 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WikEpubLib.Interfaces
+{
+    public interface IGetTocXml
+    {
+        XDocument From(IEnumerable<WikiPageRecord> pageRecords, string bookTitle);
+    }
+}
