a6794dc56a75207ea28acfba2c3e221a31adeebd
Harry Prior <harryprior@hotmail.co.uk>
2020-11-17 10:56:47 +0000

    Add class and interface for saving xml

---
+++ WikEpubLib/Interfaces/ISaveXml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WikEpubLib.Interfaces
+{
+    public interface ISaveXml
+    {
+        public Task To(string directory); 
+    }
+}
---
+++ WikEpubLib/SaveXml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikEpubLib.Interfaces;
+
+namespace WikEpubLib
+{
+    public class SaveXml : ISaveXml
+    {
+        private readonly IToc toc;
+        private readonly IContainer container;
+        private readonly IContent content;
+
+        public SaveXml(IToc toc, IContainer container, IContent content)
+        {
+            this.toc = toc;
+            this.container = container;
+            this.content = content;
+        }
+        public Task To(string directory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
