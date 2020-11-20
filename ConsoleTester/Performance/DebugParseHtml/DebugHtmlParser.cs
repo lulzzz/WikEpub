1e1802ad942ba0a7db891b6876a12bfc0cb826d8
Harry Prior <harryprior@hotmail.co.uk>
2020-11-20 18:50:24 +0000

    debug parse html

---
+++ ConsoleTester/Performance/DebugParseHtml/DebugHtmlParser.cs
@@ -0,0 +1,16 @@
+﻿using WikEpubLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsoleDebugger.Performance.DebugParseHtml
+{
+    public class DebugHtmlParser
+    {
+
+        WikEpubLib.CreateDocs.ParseHtml parseHtml = new WikEpubLib.CreateDocs.ParseHtml();
+
+    }
+}
