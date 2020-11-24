fc59ed6a7d6b36dab99cf0c1b965021983bb7938
Harry Prior <harryprior@hotmail.co.uk>
2020-11-24 10:54:29 +0000

    Remove default stuff from mvc setup

--- WikEpub/Controllers/HomeController.cs
+++ WikEpub/Controllers/HomeController.cs
@@ -23,11 +23,7 @@ namespace WikEpub.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
