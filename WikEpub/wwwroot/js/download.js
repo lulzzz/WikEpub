6fb225a3e24b7412771594d633ad5b6bbeefbc25
Harry Prior <harryprior@hotmail.co.uk>
2020-11-25 17:33:07 +0000

    Change name of js script to download

--- WikEpub/wwwroot/js/download.js
+++ WikEpub/wwwroot/js/download.js
@@ -2,4 +2,4 @@ var button = document.getElementById("add-button");
 button.addEventListener("click", function () {
     console.log("button clicked");
 });
-//# sourceMappingURL=site.js.map
+//# sourceMappingURL=download.js.map
