6570b57831e4a9f2300a1ca65f49f882c4aaa132
Harry Prior <harryprior@hotmail.co.uk>
2020-11-25 10:30:35 +0000

    Add model for downloads

---
+++ WikEpub/Models/DownloadModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikEpub.Models
+{
+    public class DownloadModel
+    {
+        public string BookTitle { get; set; }
+        public IEnumerable<string> WikiPages { get; set; }
+        public Guid guid { get; set; }
+
+        
+    }
+}
