ff9f800e0714a0dce2040a051bf08676aff191bc
Harry Prior <harryprior@hotmail.co.uk>
2020-11-30 12:11:39 +0000

    Remove changes to link request validator

--- WikEpub/wwwroot/js/LinkRequestValidator.js
+++ WikEpub/wwwroot/js/LinkRequestValidator.js
@@ -1,9 +1,3 @@
 class LinkRequestValidator {
-    constructor(parentNode, nodeIndex) {
-        this.parentNode = parentNode;
-        this.nodeIndex = nodeIndex;
-        this.nodeNum = 1;
-        addEventListener;
-    }
 }
 //# sourceMappingURL=LinkRequestValidator.js.map
