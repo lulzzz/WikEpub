f42296414fe71e452969da29de3e20bbaaf94f12
Harry Prior <harryprior@hotmail.co.uk>
2020-12-02 14:58:26 +0000

    Added skeleton methods for checking url string of input node

--- WikEpub/wwwroot/js/ValidateUrls.js
+++ WikEpub/wwwroot/js/ValidateUrls.js
@@ -1,30 +1,19 @@
 export class ValidateUrls {
     constructor(requestValidator) {
         this.requestValidator = requestValidator;
-        this.nodesAreValid = new Event('nodesAreValid');
-        this.nodesAreInvalid = new Event('nodesAreInvalid');
-        document.addEventListener('inputChange', () => this.CheckNodesUponChange());
     }
-    CheckNodesUponChange() {
-        let nodes = document.getElementsByClassName("url-input");
-        if (this.allNodesAreValid(nodes))
-            document.dispatchEvent(this.nodesAreValid);
-        else
-            document.dispatchEvent(this.nodesAreInvalid);
+    async NodeIsValid(node) {
+        let urlString = node.nodeValue;
+        if (this.urlIsValid(urlString)) {
+            return await this.CheckUrlResponse(urlString);
+        }
+        return false;
     }
-    NodeIsValid(node) {
-        // check regex
-        // make http request and check response
+    urlIsValid(url) {
         return true;
     }
-    allNodesAreValid(nodes) {
-        let numberOfNodes = nodes.length;
-        let numOfValidatedNodes = 0;
-        for (var i = 0; i < numberOfNodes; i++) {
-            if (this.NodeIsValid(nodes[i]))
-                numberOfNodes++;
-        }
-        return numOfValidatedNodes === numberOfNodes;
+    async CheckUrlResponse(url) {
+        return true;
     }
 }
 //# sourceMappingURL=ValidateUrls.js.map
--- WikEpub/wwwroot/js/ValidateUrls.ts
+++ WikEpub/wwwroot/js/ValidateUrls.ts
@@ -2,45 +2,28 @@
 
 export class ValidateUrls {
     private requestValidator: ILinkRequestValidator;
-    private nodesAreValid: Event;
-    private nodesAreInvalid: Event;
 
     constructor(requestValidator: ILinkRequestValidator) {
         this.requestValidator = requestValidator;
-        this.nodesAreValid = new Event('nodesAreValid');
-        this.nodesAreInvalid = new Event('nodesAreInvalid')
-        document.addEventListener('inputChange', () => this.CheckNodesUponChange());
     }
 
-
-    private CheckNodesUponChange() {
-        let nodes = document.getElementsByClassName("url-input");
-        if (this.allNodesAreValid(nodes)) document.dispatchEvent(this.nodesAreValid);
-        else document.dispatchEvent(this.nodesAreInvalid);
-    } 
-
-
-    private NodeIsValid(node: Node): boolean {
-        // check regex
-        // make http request and check response
-        return true;
-
-    }
-
-
-
-    private allNodesAreValid(nodes : HTMLCollectionOf<Element>) {
-        let numberOfNodes = nodes.length;
-        let numOfValidatedNodes = 0;
-        for (var i = 0; i < numberOfNodes; i++) {
-            if (this.NodeIsValid(nodes[i]))
-                numberOfNodes++;
+    public async NodeIsValid(node: Node): Promise<boolean> {
+        let urlString = node.nodeValue;
+        if (this.urlIsValid(urlString)) {
+            return await this.CheckUrlResponse(urlString)
         }
-        return numOfValidatedNodes === numberOfNodes;
-        
+        return false;
     }
 
-    
+    private urlIsValid(url: string): boolean {
+        return true 
+    }
+
+    private async CheckUrlResponse(url: string): Promise<boolean> {
+        return true
+    }
+
+
 
 }
 
