e69f0a12f5ad061aa4c18989698b7b01622cfde8
Harry Prior <harryprior@hotmail.co.uk>
2020-12-02 13:30:55 +0000

    Extract logic of event handling out of class

--- WikEpub/wwwroot/js/InputManager.js
+++ WikEpub/wwwroot/js/InputManager.js
@@ -3,29 +3,23 @@ export class InputManager {
         this.parentNode = parentNode;
         this.nodeIndex = nodeIndex;
         this.nodeNum = 1;
-        // this will be extracted into the class using this one
-        let addButton = document.getElementById("add-button");
-        let removeButton = document.getElementById("remove-button");
-        addButton.addEventListener("click", () => this.insertInput("p"));
-        removeButton.addEventListener("click", () => this.removeInput());
-        this.inputChangeEvent = new Event('inputChange');
     }
     insertInput(enclosingNodeType) {
         if (this.nodeNum > 9)
-            return;
+            return null;
         this.nodeIndex++;
         this.nodeNum++;
         let insertNode = this.createInputNode(enclosingNodeType);
         this.insertAfter(this.parentNode.childNodes[this.nodeIndex], insertNode);
-        document.dispatchEvent(this.inputChangeEvent);
+        return insertNode;
     }
     removeInput() {
         if (this.nodeNum === 1)
-            return;
+            return false;
         this.parentNode.childNodes[this.nodeIndex].remove();
         this.nodeIndex--;
         this.nodeNum--;
-        document.dispatchEvent(this.inputChangeEvent);
+        return true;
     }
     insertAfter(newSiblingNode, newNode) {
         newSiblingNode.parentNode.insertBefore(newNode, newSiblingNode);
@@ -33,9 +27,10 @@ export class InputManager {
     }
     createInputNode(enclosingNodeType) {
         let enclosingNode = document.createElement(enclosingNodeType);
+        enclosingNode.id = "input-frame-" + (this.nodeNum).toString();
         let inputNode = document.createElement("input");
         inputNode.setAttribute("name", "WikiPages");
-        inputNode.setAttribute("id", "input" + (this.nodeIndex).toString());
+        inputNode.setAttribute("id", "input" + (this.nodeNum).toString());
         inputNode.className = "url-input";
         enclosingNode.textContent = "Wikipedia url: ";
         enclosingNode.appendChild(inputNode);
--- WikEpub/wwwroot/js/InputManager.ts
+++ WikEpub/wwwroot/js/InputManager.ts
@@ -3,35 +3,27 @@ import { IManageInputs } from "./Interfaces/IManageInputs";
 
 export class InputManager implements IManageInputs {
     private nodeNum = 1;
-    private inputChangeEvent: Event;
 
     constructor(
         private parentNode: Node,
         private nodeIndex: number
-    ) {
-        // this will be extracted into the class using this one
-        let addButton = document.getElementById("add-button");
-        let removeButton = document.getElementById("remove-button");
-        addButton.addEventListener("click", () => this.insertInput("p"));
-        removeButton.addEventListener("click", () => this.removeInput());
-        this.inputChangeEvent = new Event('inputChange');
-    }
+    ){}
 
-    public insertInput(enclosingNodeType: string): void {
-        if (this.nodeNum > 9) return;
+    public insertInput(enclosingNodeType: string): Node{
+        if (this.nodeNum > 9) return null;
         this.nodeIndex++;
         this.nodeNum++;
         let insertNode = this.createInputNode(enclosingNodeType);
         this.insertAfter(this.parentNode.childNodes[this.nodeIndex], insertNode);
-        document.dispatchEvent(this.inputChangeEvent);
+        return insertNode;
     }
 
-    public removeInput() : void {
-        if (this.nodeNum === 1) return;
+    public removeInput(): boolean {
+        if (this.nodeNum === 1) return false;
         this.parentNode.childNodes[this.nodeIndex].remove();
         this.nodeIndex--;
         this.nodeNum--;
-        document.dispatchEvent(this.inputChangeEvent);
+        return true;
     }
 
     private insertAfter(newSiblingNode: Node, newNode: Node): Node {
@@ -41,9 +33,10 @@ export class InputManager implements IManageInputs {
 
     private createInputNode(enclosingNodeType: string): Node {
         let enclosingNode = document.createElement(enclosingNodeType);
+        enclosingNode.id = "input-frame-" + (this.nodeNum).toString();
         let inputNode = document.createElement("input");
         inputNode.setAttribute("name", "WikiPages");
-        inputNode.setAttribute("id", "input" + (this.nodeIndex).toString());
+        inputNode.setAttribute("id", "input" + (this.nodeNum).toString());
         inputNode.className = "url-input";
         enclosingNode.textContent = "Wikipedia url: ";
         enclosingNode.appendChild(inputNode);
