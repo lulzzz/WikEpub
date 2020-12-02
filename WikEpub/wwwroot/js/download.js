ddae0c909d11689710a116c4b176eb28a417442d
Harry Prior <harryprior@hotmail.co.uk>
2020-12-02 13:30:39 +0000

    Refactor input manager's event handling to main class

--- WikEpub/wwwroot/js/download.js
+++ WikEpub/wwwroot/js/download.js
@@ -1,4 +1,29 @@
 import { InputManager } from "./InputManager.js";
+class DownloadPageManager {
+    constructor(inputManager) {
+        this.nodes = [];
+        this.inputManager = inputManager;
+        this.inputValidator = this.inputValidator;
+        this.nodes.push(document.getElementById("input-frame-1")); // first node
+        this.SetUpButtons();
+    }
+    SetUpButtons() {
+        let addButton = document.getElementById("add-button");
+        let removeButton = document.getElementById("remove-button");
+        addButton.addEventListener('click', () => {
+            let newNode = this.inputManager.insertInput('p'); // side-effect on DOM
+            if (newNode !== null) {
+                this.nodes.push(newNode);
+            }
+            console.log(this.nodes.length.toString());
+        });
+        removeButton.addEventListener('click', () => {
+            if (this.inputManager.removeInput())
+                this.nodes.pop(); // side-effect on DOM
+            console.log(this.nodes.length.toString());
+        });
+    }
+}
 let urlInputEventHandler = new InputManager(document.getElementById("main-form"), 3);
-//let urlValidator = new ValidateUrls(document.getElementsByClassName("url-input"));
+let pageManager = new DownloadPageManager(urlInputEventHandler);
 //# sourceMappingURL=Download.js.map
--- WikEpub/wwwroot/js/download.ts
+++ WikEpub/wwwroot/js/download.ts
@@ -1,13 +1,46 @@
 ﻿import { InputManager } from "./InputManager.js";
+import { ValidateUrls } from "./ValidateUrls.js";
+import { IManageInputs } from "./Interfaces/IManageInputs"
+import { ILinkRequestValidator } from "./Interfaces/ILinkRequestValidator";
 
+
+
+
+
+class DownloadPageManager {
+    inputManager: IManageInputs;
+    inputValidator: ILinkRequestValidator; 
+    nodes: Node[];
+
+    constructor(inputManager: IManageInputs) {
+        this.nodes = [];
+        this.inputManager = inputManager;
+        this.inputValidator = this.inputValidator;
+        this.nodes.push(document.getElementById("input-frame-1")); // first node
+        this.SetUpButtons();
+    }
+
+    private SetUpButtons(): void{
+        let addButton = document.getElementById("add-button");
+        let removeButton = document.getElementById("remove-button");
+        addButton.addEventListener('click', () => {
+            let newNode = this.inputManager.insertInput('p'); // side-effect on DOM
+            if (newNode !== null) {
+                this.nodes.push(newNode);
+            }
+            console.log(this.nodes.length.toString())
+        });
+        removeButton.addEventListener('click', () => {
+            if (this.inputManager.removeInput()) this.nodes.pop(); // side-effect on DOM
+            console.log(this.nodes.length.toString())
+        });
+
+    }
+}
 
 let urlInputEventHandler: InputManager = new InputManager(document.getElementById("main-form"), 3);
 
-
-
-
-
-//let urlValidator = new ValidateUrls(document.getElementsByClassName("url-input"));
+let pageManager = new DownloadPageManager(urlInputEventHandler);
 
 
  
