d0a987775ea494227d8c8fcb53014f3d5b60526e
Harry Prior <harryprior@hotmail.co.uk>
2020-12-03 17:47:31 +0000

    Add link request validator logic

--- WikEpub/wwwroot/js/LinkRequestValidator.js
+++ WikEpub/wwwroot/js/LinkRequestValidator.js
@@ -1,6 +1,13 @@
 export class LinkRequestValidator {
     async ValidateLink(url) {
-        return await true;
+        let response = await fetch(url, {
+            mode: 'cors',
+            headers: { 'origin': 'include' }
+        });
+        if (response.status === 200) {
+            return true;
+        }
+        return false;
     }
 }
 //# sourceMappingURL=LinkRequestValidator.js.map
--- WikEpub/wwwroot/js/LinkRequestValidator.ts
+++ WikEpub/wwwroot/js/LinkRequestValidator.ts
@@ -1,6 +1,14 @@
 ﻿import { ILinkRequestValidator } from "./Interfaces/ILinkRequestValidator";
 export class LinkRequestValidator implements ILinkRequestValidator {
     public async ValidateLink(url: string): Promise<boolean> {
-        return await true;
+        let response: Response = await fetch(url, {
+            mode: 'cors',
+            headers: {'origin': 'include'}
+                }
+            );
+        if (response.status === 200) {
+            return true;
+        }
+        return false;
     }
 }
