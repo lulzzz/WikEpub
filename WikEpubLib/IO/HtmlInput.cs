7d4d90220e22c0e74f8267f24f30c6a0ee4f6548
Harry Prior <harryprior@hotmail.co.uk>
2020-12-05 14:58:51 +0000

    Add exception to catch invalid urls Add regex to filter urls

--- WikEpubLib/IO/HtmlInput.cs
+++ WikEpubLib/IO/HtmlInput.cs
@@ -1,16 +1,31 @@
 ﻿using HtmlAgilityPack;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using WikEpubLib.Exceptions;
 
 namespace WikEpubLib.IO
 {
     public class HtmlInput : IHtmlInput
     {
-        public async Task<HtmlDocument[]> GetHtmlDocumentsFromAsync(IEnumerable<string> urls, HtmlWeb htmlWeb) =>
-            await Task.WhenAll(urls.Select(url => htmlWeb.LoadFromWebAsync(TranslateToApiCall(url))));
+        public async Task<HtmlDocument[]> GetHtmlDocumentsFromAsync(IEnumerable<string> urls, HtmlWeb htmlWeb)
+        {
+            if (UrlsAreValid(urls))
+                await Task.WhenAll(urls.Select(url => htmlWeb.LoadFromWebAsync(TranslateToApiCall(url))));
 
+            throw new InvalidWikiUrlException(urls);
+        }
         private string TranslateToApiCall(string url) =>
             $@"https://en.wikipedia.org/api/rest_v1/page/html/{url.Split('/').Last()}";
+
+        private bool UrlsAreValid(IEnumerable<string> urls)
+        {
+            Regex regex = new Regex("(https:\\/\\/)?(en\\.)?wikipedia\\.org\\/[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&\\/\\/=,]*)");
+            foreach(var url in urls)
+                if (!regex.IsMatch(url))
+                    return false;
+            return true;
+        }
     }
 }
---
+++ WikEpubLibTests/HtmlInputTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikEpubLib.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WikEpubLibTests
+{
+    [TestClass]
+    public class HtmlInputTest
+    {
+        [TestMethod]
+        public void Valid_Url_Does_Not_Throw_Exception()
+        {
+
+        }
+    }
+}
