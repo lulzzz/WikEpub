f1465eae95e63107c695e3431becc164aa5be49a
Harry Prior <harryprior@hotmail.co.uk>
2020-12-05 14:58:29 +0000

    Add exception to to catch invalid wiki on server side

---
+++ WikEpubLib/Exceptions/InvalidWikiUrlException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikEpubLib.Exceptions
+{
+    public class InvalidWikiUrlException : Exception
+    {
+        public InvalidWikiUrlException(IEnumerable<string> urls) : base($"Invalid urls: {urls}")
+        { }
+                
+    }
+}
