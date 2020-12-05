b2642453281d87f2e616bbf747b375358de3c2da
Harry Prior <harryprior@hotmail.co.uk>
2020-12-05 16:56:58 +0000

    Add tests for throwing exception with invalid url

--- WikEpubLibTests/HtmlInputTest.cs
+++ WikEpubLibTests/HtmlInputTest.cs
@@ -5,16 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 using WikEpubLib.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HtmlAgilityPack;
+using WikEpubLib.Exceptions;
 
 namespace WikEpubLibTests
 {
     [TestClass]
     public class HtmlInputTest
     {
-        [TestMethod]
-        public void Valid_Url_Does_Not_Throw_Exception()
+        HtmlWeb htmlWeb;
+        HtmlInput htmlInput;
+        [TestInitialize]
+        public void Init()
         {
-
+            htmlInput = new HtmlInput();
+            htmlWeb = new HtmlWeb();
         }
+        [TestMethod]
+        public async Task Valid_Url_Does_Not_Throw_Exception()
+        {
+            List<string> validUrls = new()
+            {
+                "wikipedia.org/wiki/Thomas_Kuhn",
+                "en.wikipedia.org/wiki/Thomas_Kuhn",
+                "https://en.wikipedia.org/wiki/Thomas_Kuhn"
+            };
+            await htmlInput.GetHtmlDocumentsFromAsync(validUrls, htmlWeb);
+        }
+        
+        [TestMethod]
+        public async Task Url_Throws_Exception_No_Title()
+        {
+            List<string> validUrls = new()
+            {
+                "https://en.wikipedia.org/wiki/"
+            };
+            await Assert.ThrowsExceptionAsync<InvalidWikiUrlException>(
+                () => htmlInput.GetHtmlDocumentsFromAsync(validUrls, htmlWeb)
+                );
+        }
+
+        [TestMethod]
+        public async Task Url_Throws_Exception()
+        {
+            List<string> validUrls = new()
+            {
+                "https://en.wikipedia.org/wiki/"
+            };
+            await htmlInput.GetHtmlDocumentsFromAsync(validUrls, htmlWeb);
+        }
+
+
     }
 }
