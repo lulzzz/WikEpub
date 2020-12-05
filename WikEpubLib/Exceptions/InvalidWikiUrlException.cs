d4b0b28085fd0262155499a21e3590c83e0e3b34
Harry Prior <harryprior@hotmail.co.uk>
2020-12-05 16:56:21 +0000

    Print out all urls in exception and highlight invalid ones

--- WikEpubLib/Exceptions/InvalidWikiUrlException.cs
+++ WikEpubLib/Exceptions/InvalidWikiUrlException.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WikEpubLib.Exceptions
 {
     public class InvalidWikiUrlException : Exception
     {
-        public InvalidWikiUrlException(IEnumerable<string> urls) : base($"Invalid urls: {urls}")
+        static Regex regex = new Regex("(https:\\/\\/)?(en\\.)?wikipedia\\.org\\/(wiki\\/\\b(([-a-zA-Z0-9()@:%_\\+.~#?&\\/\\/=,]*){1}))");
+        public InvalidWikiUrlException(IEnumerable<string> urls) : 
+            base($"Invalid urls: {urls.Select(x => regex.IsMatch(x) ? x : $"-->{x}<--").Aggregate((x, y) =>  x + "\n"+ y)}")
         { }
                 
     }
