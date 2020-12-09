6cb704ba69a4d5ad048f79e52f5fbfc102e71075
Harry Prior <harryprior@hotmail.co.uk>
2020-12-09 12:22:13 +0000

    Add input validator class

---
+++ WikEpub/wwwroot/js/Interfaces/IInputValidator.js
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=IInputValidator.js.map
---
+++ WikEpub/wwwroot/js/Interfaces/IInputValidator.ts
@@ -0,0 +1,7 @@
+﻿export interface IInputValidator {
+    AddNode(node: Node): void;
+    RemoveNode(): void;
+    CheckNode(node: Node): boolean;
+    AllNodesAreValid(): boolean;
+    GetValidNodeReason(node: Node): [boolean, string];
+}
