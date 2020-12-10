d719621c12ab20430bac1f32e3cf428ff61f71b7
Harry Prior <harryprior@hotmail.co.uk>
2020-12-10 14:12:25 +0000

    Add download form script which instantiates classes for input creation and form validation

---
+++ WikEpub/wwwroot/js/DownloadForm.ts
@@ -0,0 +1,9 @@
+﻿import { DownloadPageManager } from "./DownloadManager.js";
+import { InputValidator } from "./InputValidator.js";
+import { InputManager } from "./InputManager.js";
+import { ValidateUrls } from "./ValidateUrls.js";
+
+let validateUrls = new ValidateUrls();
+let inputManager = new InputManager(document.getElementById("main-form"));
+let inputValidator = new InputValidator(validateUrls);
+let downloadPageManager = new DownloadPageManager(inputManager, inputValidator);
