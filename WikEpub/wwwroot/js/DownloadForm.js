6cf824ebef2195c25b67beb5383d696d26aa7ed6
Harry Prior <harryprior@hotmail.co.uk>
2020-12-11 13:36:30 +0000

    Change InputManager class constructor argument to new container for url links

--- WikEpub/wwwroot/js/DownloadForm.js
+++ WikEpub/wwwroot/js/DownloadForm.js
@@ -3,7 +3,7 @@ import { InputValidator } from "./InputValidator.js";
 import { InputManager } from "./InputManager.js";
 import { ValidateUrls } from "./ValidateUrls.js";
 let validateUrls = new ValidateUrls();
-let inputManager = new InputManager(document.getElementById("main-form"));
+let inputManager = new InputManager(document.getElementById("url-link-container"));
 let inputValidator = new InputValidator(validateUrls);
 let downloadPageManager = new DownloadPageManager(inputManager, inputValidator);
 //# sourceMappingURL=DownloadForm.js.map
--- WikEpub/wwwroot/js/DownloadForm.ts
+++ WikEpub/wwwroot/js/DownloadForm.ts
@@ -4,6 +4,6 @@ import { InputManager } from "./InputManager.js";
 import { ValidateUrls } from "./ValidateUrls.js";
 
 let validateUrls = new ValidateUrls();
-let inputManager = new InputManager(document.getElementById("main-form"));
+let inputManager = new InputManager(document.getElementById("url-link-container"));
 let inputValidator = new InputValidator(validateUrls);
 let downloadPageManager = new DownloadPageManager(inputManager, inputValidator);
